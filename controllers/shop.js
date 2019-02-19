2437e939771292beff8027daafe45703b73934bb
seyhagithub <hiemseyha168@gmail.com>
2019-02-20 00:18:15 +0700

    one pdfkit

--- controllers/shop.js
+++ controllers/shop.js
@@ -155,14 +155,21 @@ exports.getInvoice = (req, res, next) => {
 	}
 	const invoiceName = 'invoice-' + orderId + '.pdf';
   	const invoicePath = path.join('data', 'invoices', invoiceName);
-	fs.readFile(invoicePath, (error, data) => {
-		if(error) {
-			return next(error);
-		}
-		res.setHeader('Content-Type', 'application/pdf');
-		res.setHeader('Content-Disposition', 'inline; filename="' + invoiceName + '"');
-		res.send(data);
-	});
+	// fs.readFile(invoicePath, (error, data) => {
+	// 	if(error) {
+	// 		return next(error);
+	// 	}
+	// 	res.setHeader('Content-Type', 'application/pdf');
+	// 	res.setHeader('Content-Disposition', 'inline; filename="' + invoiceName + '"');
+	// 	res.send(data);
+	// });
+	console.log(invoiceName);
+	console.log('reading steam file');
+	const file = fs.createReadStream(invoicePath);
+	res.setHeader('Content-Type', 'application/pdf');
+	res.setHeader('Content-Disposition', 
+	'inline; filename="' + invoiceName + '"');
+	file.pipe(res);
   }).catch(err => next(err));
 
   
